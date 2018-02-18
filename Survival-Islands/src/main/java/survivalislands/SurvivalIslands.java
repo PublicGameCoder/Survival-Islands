4381ef84f6bba266565b35be6a8220fc68d1a827
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-02-18 10:02:06 +0100

    Add SurvivalIslands (main class)
    Signed-off-by: PublicGameCoder <mikedegroot1999@hotmail.com>

---
+++ Survival-Islands/src/main/java/survivalislands/SurvivalIslands.java
@@ -0,0 +1,21 @@
+package survivalislands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SurvivalIslands extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+		// TODO Loading configuration
+	}
+	
+	@Override
+	public void onEnable() {
+		// TODO Setting configuration
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Saving configuration
+	}
+}
