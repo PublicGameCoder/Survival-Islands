f8a17e5bef1e640ac04fe1c33090f6f98c32d5a9
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-02-21 11:20:40 +0100

    Implement Managers initialization

--- Survival-Islands/src/main/java/survivalislands/SurvivalIslands.java
+++ Survival-Islands/src/main/java/survivalislands/SurvivalIslands.java
@@ -3,9 +3,14 @@ package survivalislands;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import managers.CMDManager;
+import managers.ConfigManager;
+import managers.PlayerStatsManager;
+import managers.WorldProcessInteractor;
 
 public class SurvivalIslands extends JavaPlugin {
 
+	private static SurvivalIslands instance;
+	
 	@Override
 	public void onLoad() {
 		// TODO Loading configuration
@@ -14,14 +19,25 @@ public class SurvivalIslands extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		// TODO Setting configuration
+		instance = this;
+		
 		getCommand("SurvivalIsland").setExecutor(CMDManager.getManager());
 		getCommand("SI").setExecutor(CMDManager.getManager());
 		getCommand("SIM").setExecutor(CMDManager.getManager());
 		getCommand("SIR").setExecutor(CMDManager.getManager());
+		
+		//loading manager(s)
+		ConfigManager.getManager();
+		PlayerStatsManager.getManager();
+		WorldProcessInteractor.getManager();
 	}
 	
 	@Override
 	public void onDisable() {
 		// TODO Saving configuration
+	}
+	
+	public static SurvivalIslands getInstance() {
+		return instance;
 	}
 }
